817ff248af2cb97cca2d0685f546241e8f050f69
shreyaspalan <30557654+shreyaspalan@users.noreply.github.com>
2019-01-31 08:57:34 +0530

    Add files via upload

---
+++ p2/pattern2.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp1
+{
+    class Class1
+    {
+        static void Main(string[] args) {
+
+            String s1;
+            Console.WriteLine("size of an array");
+            s1= Console.ReadLine();
+        
+            int i, j;
+            for (i = 1; i <= 4; i++)
+            {
+                for (j = 1; j <= i; j++)
+                {
+                    Console.Write(j + " ");
+                }
+
+                Console.WriteLine("");
+            }
+            Console.ReadKey();
+
+
+
+        }
+    }
+}
